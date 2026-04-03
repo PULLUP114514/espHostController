e57980cbfaa041294dbcbd42b78746b2c81a04f6
PULLUP114514 <persoutsink@outlook.com>
2026-04-03 09:13:27 +0800

    fix:单播和广播识别问题

--- EspNowController/main/NowMode.c
+++ EspNowController/main/NowMode.c
@@ -9,12 +9,11 @@
 #include "esp_mac.h"
 #include "esp_netif.h"
 #include "esp_event.h"
-
+#include "EspnowConfig.h"
 #define TAG "ESP_DISCOVER"
 
 #define CHANNEL 1
 #define SEND_INTERVAL 1000
-
 #define MAX_PEERS 20
 
 static void SendImg(void *pvParameters);
@@ -27,7 +26,7 @@ typedef struct
         uint8_t payload[64];
 } packet_t;
 
-static uint8_t broadcast_mac[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+static uint8_t broadcastMac[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
 /* ===== 设备列表 ===== */
 static uint8_t discovered_macs[MAX_PEERS][6];
@@ -54,7 +53,12 @@ static bool add_mac(uint8_t *mac)
         {
                 memcpy(discovered_macs[discovered_count], mac, 6);
                 discovered_count++;
-
+                esp_now_peer_info_t peer;
+                memcpy(peer.peer_addr, mac, 6);
+                peer.channel = CHANNEL;      // 0 表示当前信道（推荐）
+                peer.ifidx = ESPNOW_WIFI_IF; // 一般用 STA
+                peer.encrypt = false;        // 是否加密
+                esp_now_add_peer(&peer);
                 ESP_LOGI(TAG, "发现新设备: " MACSTR, MAC2STR(mac));
                 return true;
         }
@@ -80,28 +84,32 @@ static void wifi_init(void)
 /* ===== 接收回调 ===== */
 static void recv_cb(const esp_now_recv_info_t *info, const uint8_t *data, int len)
 {
-        uint8_t *mac = info->src_addr;
+        uint8_t *srcMac = info->src_addr;
         packet_t *pkt = (packet_t *)data;
         packet_t outpkt = {0};
         outpkt.packageId = 0;
-        if (mac == NULL || data == NULL || len <= 0)
+        if (srcMac == NULL || data == NULL || len <= 0)
         {
                 return;
         }
-
-        switch (pkt->type)
+        if (IS_BROADCAST_ADDR(info->des_addr))
         {
-        case 0:
                 /* 回发广播（让对方发现自己） */
-                // ESP_LOGI(TAG, "收到广播来自: " MACSTR, MAC2STR(mac));
+                ESP_LOGI(TAG, "收到广播来自: " MACSTR, MAC2STR(srcMac));
                 outpkt.type = 1;
                 memcpy(outpkt.payload, "HELLO", 6);
-                esp_now_send(broadcast_mac, (uint8_t *)&outpkt, sizeof(outpkt));
-                break;
+                esp_now_send(srcMac, (uint8_t *)&outpkt, sizeof(outpkt));
+        }
+        else
+        {
+                ESP_LOGI(TAG, "收到单播来自: " MACSTR, MAC2STR(srcMac));
+        }
+        switch (pkt->type)
+        {
         case 2:
                 ESP_LOGI(TAG, "收到普通数据：%s", (char *)(pkt->payload));
                 outpkt.type = 3;
-                esp_now_send(broadcast_mac, (uint8_t *)&outpkt, sizeof(outpkt));
+                esp_now_send(srcMac, (uint8_t *)&outpkt, sizeof(outpkt));
                 break;
         case 3:
                 ESP_LOGI(TAG, "ACK");
@@ -109,14 +117,14 @@ static void recv_cb(const esp_now_recv_info_t *info, const uint8_t *data, int le
                 break;
         }
 
-        bool newMac = add_mac(mac);
+        bool newMac = add_mac(srcMac);
         if (newMac)
         {
                 xTaskCreate(
                     SendImg,       // 任务函数
                     "SendImgTask", // 名字
                     4096,          // 栈大小
-                    NULL,          // 参数（传指针）
+                    srcMac,        // 参数（传指针）
                     5,             // 优先级
                     NULL           // 任务句柄
                 );
@@ -125,13 +133,15 @@ static void recv_cb(const esp_now_recv_info_t *info, const uint8_t *data, int le
 
 static void SendImg(void *pvParameters)
 {
+
+        uint8_t *mac = (uint8_t *)pvParameters;
         while (true)
         {
                 packet_t pkt = {0};
                 pkt.type = 2;
                 pkt.packageId = 0;
                 memcpy(pkt.payload, "GETGETGET12313123123", 21);
-                esp_now_send(broadcast_mac, (uint8_t *)&pkt, sizeof(pkt));
+                esp_now_send(mac, (uint8_t *)&pkt, sizeof(pkt));
                 if (ACKFlag == true)
                 {
                         ACKFlag = false;
@@ -165,7 +175,7 @@ static void espnow_init(void)
 
         /* 添加广播 peer */
         esp_now_peer_info_t peer = {0};
-        memcpy(peer.peer_addr, broadcast_mac, 6);
+        memcpy(peer.peer_addr, broadcastMac, 6);
         peer.channel = CHANNEL;
         peer.ifidx = WIFI_IF_STA;
         peer.encrypt = false;
@@ -182,7 +192,7 @@ static void broadcast_task(void *arg)
                 pkt.type = 0;
                 memcpy(pkt.payload, "DISCOVER", 9);
 
-                esp_err_t err = esp_now_send(broadcast_mac, (uint8_t *)&pkt, sizeof(pkt));
+                esp_err_t err = esp_now_send(broadcastMac, (uint8_t *)&pkt, sizeof(pkt));
 
                 if (err != ESP_OK)
                 {
